03ef0228237b8ad38ec72f399ceab02321e3fd61
kbventures <kmb_x@hotmail.com>
2021-01-11 16:58:01 +0100

    Return part of an array using slice method

---
+++ 11returnPartOfAnArrayUsingSliceMethod.js
@@ -0,0 +1,34 @@
+
+
+function sliceArray(anim, beginSlice, endSlice) {
+    // Only change code below this line
+    
+    var newArray = [];
+    newArray= anim.slice(beginSlice, endSlice);
+
+    return newArray; 
+  
+    // Only change code above this line
+  }
+  var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+  sliceArray(inputAnim, 1, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+// function sliceArray(anim, beginSlice, endSlice) {
+//     // Only change code below this line
+  
+  
+//     // Only change code above this line
+//   }
+//   var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+//   sliceArray(inputAnim, 1, 3);
